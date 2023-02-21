6752fe4efede97892a0020150206959c372c9f39
Jelenko76 <andreasjelenko76@hotmail.com>
2023-02-21 10:28:11 +0000

    last commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// creates to random numbers between 0 and 1
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+  console.log(getRandomInt(2));
+  console.log(getRandomInt(1));
+  console.log(Math.random());
